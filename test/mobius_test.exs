ea72247252cfa486d5dd3796d3a3e9db892a4dc6
Mike <mdwaud@gmail.com>
2021-11-06 10:56:54 -0400

    ignore bad history file

--- test/mobius_test.exs
+++ test/mobius_test.exs
@@ -1,2 +1,33 @@
 defmodule MobiusTest do
+  use ExUnit.Case, async: false
+  import ExUnit.CaptureLog
+
+  @persistence_dir System.tmp_dir!() |> Path.join("mobius_test")
+  @default_args [
+    persistence_dir: @persistence_dir,
+    metrics: []
+  ]
+
+  setup do
+    File.rm_rf!(@persistence_dir)
+    File.mkdir_p(@persistence_dir)
+  end
+
+  test "starts" do
+    assert {:ok, _pid} = start_supervised({Mobius, @default_args})
+  end
+
+  test "does not crash with a corrupt history file" do
+    persistence_path = Path.join(@persistence_dir, "mobius")
+    File.mkdir_p(persistence_path)
+    File.write!(file(persistence_path), <<>>)
+
+    assert capture_log(fn ->
+             assert {:ok, _pid} = start_supervised({Mobius, @default_args})
+           end) =~ "Error reading history file"
+  end
+
+  defp file(persistence_dir) do
+    Path.join(persistence_dir, "history")
+  end
 end
