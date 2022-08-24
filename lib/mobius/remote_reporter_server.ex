4dd9d78e5266a91ee4c81770e195dc85550a80fd
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-08-24 14:49:11 -0700

    Support on demand reports
    Update the remote reporter logic to allow for programmatically or
    manually sending remote reports when requested.
    
    This breaks the default behavior of sending reports every minute if no
    interval is provided. To upgrade a user has to explicitly set the report
    interval, otherwise Mobius will not automatically send reports.
    
    This give more control over data usage for devices that use mobile
    connects or systems that need to ingest data on demand rather than in real
    time.

--- lib/mobius/remote_reporter_server.ex
+++ lib/mobius/remote_reporter_server.ex
@@ -34,19 +34,21 @@ defmodule Mobius.RemoteReporterServer do
     instance = args[:mobius_instance] || :mobius
     {reporter, reporter_args} = get_reporter(args)
     {:ok, state} = reporter.init(reporter_args)
-    report_interval = args[:report_interval] || 60_000
+    start_time = System.monotonic_time(:second)
+    report_interval = args[:report_interval]
 
-    timer_ref = Process.send_after(self(), :report, report_interval)
+    state =
+      %{
+        reporter: reporter,
+        reporter_state: state,
+        report_interval: report_interval,
+        next_query_from: nil,
+        mobius: instance,
+        start_time: start_time
+      }
+      |> maybe_start_interval()
 
-    {:ok,
-     %{
-       reporter: reporter,
-       reporter_state: state,
-       report_interval: report_interval,
-       interval_ref: timer_ref,
-       next_query_from: nil,
-       mobius: instance
-     }}
+    {:ok, state}
   end
 
   defp get_reporter(args) do
@@ -56,39 +58,87 @@ defmodule Mobius.RemoteReporterServer do
     end
   end
 
+  @doc """
+  Report the latest metrics
+
+  This function will for the latest metrics to be reported. This is useful for
+  programmatic or manually control for metrics reporting.
+  """
+  @spec report_metrics(Mobius.instance()) :: :ok
+  def report_metrics(mobius_instance \\ :mobius) do
+    name = name(mobius_instance)
+
+    GenServer.cast(name, :report_metrics)
+  end
+
+  @impl GenServer
+  def handle_cast(:report_metrics, state) do
+    {:noreply, run_report(state)}
+  end
+
   @impl GenServer
   def handle_info(:report, state) do
+    {:noreply, run_report(state)}
+  end
+
+  defp run_report(state) do
     {from, to} = get_query_window(state)
     records = Scraper.all(state.mobius, from: from, to: to)
-    new_timer_ref = Process.send_after(self(), :report, state.report_interval)
 
     case state.reporter.handle_metrics(records, state.reporter_state) do
       {:noreply, new_state} ->
-        {:noreply,
-         %{
-           state
-           | reporter_state: new_state,
-             next_query_from: to + 1,
-             interval_ref: new_timer_ref
-         }}
+        %{
+          state
+          | reporter_state: new_state,
+            next_query_from: to + 1
+        }
+        |> maybe_start_interval()
 
       {:error, _reason, new_state} ->
-        {:noreply, %{state | reporter_state: new_state, interval_ref: new_timer_ref}}
+        state = %{state | reporter_state: new_state}
+
+        maybe_start_interval(state)
     end
   end
 
-  def get_query_window(%{next_query_from: nil} = state) do
+  # if there is not a report interval and a report has not been sent yet
+  defp get_query_window(%{report_interval: nil, next_query_from: nil} = state) do
+    now_monotonic_time = System.monotonic_time(:second)
+    # the amount of time (seconds) that has pass between starting the server
+    # wanting to send the report to calculate the from timestamp.
+    time_passed = now_monotonic_time - state.start_time
+    now_time = now()
+
+    {now_time - time_passed, now_time}
+  end
+
+  # if there is not a report interval
+  defp get_query_window(%{report_interval: nil} = state) do
+    {state.next_query_from, now()}
+  end
+
+  defp get_query_window(%{next_query_from: nil} = state) do
     now = now()
     subtract = div(state.report_interval, 1000)
 
     {now - subtract, now}
   end
 
-  def get_query_window(state) do
+  defp get_query_window(state) do
     {state.next_query_from, now()}
   end
 
   defp now() do
     DateTime.to_unix(DateTime.utc_now(), :second)
+  end
+
+  defp maybe_start_interval(state) do
+    if state.report_interval do
+      timer_ref = Process.send_after(self(), :report, state.report_interval)
+
+      Map.put(state, :interval_ref, timer_ref)
+    else
+      state
+    end
   end
 end
