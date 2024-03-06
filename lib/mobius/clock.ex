df29168f91fb63fce609089a06d6b801e757ab54
Sébastien Saint-Sevin <s.saint.sevin@gmail.com>
2024-03-06 12:06:43 +0100

    improve doc

--- lib/mobius/clock.ex
+++ lib/mobius/clock.ex
@@ -2,21 +2,21 @@ defmodule Mobius.Clock do
   @moduledoc """
   Behaviour for Mobius to check if the clock is set
 
-  On systems that need to set the time after boot events and metrics might
+  On systems that need to set the time after boot events, metrics might
   report a nonsensical timestamp. Providing a clock implementation allows Mobius
   to make time adjustments on data received before the clock was set.
 
   If no clock implementation is provided no time adjustments will be made.
 
   For Nerves devices, [NervesTime](https://hex.pm/packages/nerves_time) can be
-  used to time synchronization.
+  used to track time synchronization.
 
   ```elixir
   {Mobius, clock: NervesTime}
   ```
 
-  The time adjustments are best effort and might not 100% exact, but this should
-  only effect events that take place during the early stages of system boot.
+  The time adjustments are best effort and might not be 100% exact, but this should
+  only affect events that take place during the early stages of system boot.
   """
 
   @doc """
