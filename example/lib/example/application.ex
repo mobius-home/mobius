e091b126abeb39de6351ce9d68b28811f7d8ce31
Frank Hunleth <fhunleth@troodon-software.com>
2026-06-02 09:27:30 -0400

    Update HexDoc URL

--- example/lib/example/application.ex
+++ example/lib/example/application.ex
@@ -1,5 +1,5 @@
 defmodule Example.Application do
-  # See https://hexdocs.pm/elixir/Application.html
+  # See https://elixir.hexdocs.pm/Application.html
   # for more information on OTP Applications
   @moduledoc false
 
@@ -30,7 +30,7 @@ defmodule Example.Application do
        remote_report_interval: 10_000}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
+    # See https://elixir.hexdocs.pm/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Example.Supervisor]
     Supervisor.start_link(children, opts)
